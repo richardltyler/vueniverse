eaf5668725b344929365a0198370d2ff0bdc48c3
colefiscus <>
2021-02-27 14:00:15 -0700

    Create Routes test file and add tests for URLs

--- cypress/integration/picture-of-the-day-spec.js
+++ cypress/integration/picture-of-the-day-spec.js
@@ -1,36 +1,11 @@
-describe("Click To Enter component", () => {
+describe("Pic of the Day component", () => {
 
   beforeEach(() => {
-    cy.visit("http://localhost:8080")
-  })
-
-  it("Should visit intial component on page load", () => {
-    cy.url().should("include", "8080/")
-  })
-
-  it("Should contain text and an input in the header", () => {
     cy
-      .get("header").get("h1").contains("VUENIVERSE").should("be.visible")
-      .get("header").get("input").should("be.visible")
-      .get("header").get("p").contains("Expand your Vue.").should("be.visible")
+      visit("http://localhost:8080")
+      .get("footer").get("a:last").click()
   })
 
-  it("Should contain a blast-off button and additional text", () => {
-    cy
-      .get("main").get("button").contains("🤠 CLICK HERE TO BLAST OFF 🤠").should("be.visible")
-      .get("main").get("p").contains("(WARNING FLASHING LIGHTS!!)").should("be.visible")
-  })
+  
 
-  it("Should contain a footer with component links", () => {
-    cy
-      .get("footer").get("a:first").contains("About").should("be.visible")
-      .get("footer").get("a:last").contains("Home").should("be.visible")
-  })
-
-  it("Should show the picture of the day after clicking the blast-off button", () => {
-    cy
-      .get("#app").should("have.css", "background-image", 'url("http://localhost:8080/img/intro.24ff364e.png")')
-      .get("main").get("button").click()
-      .get("#app").should("have.css", "background-image", 'url("https://media.giphy.com/media/MaThe6p8WAKbf9NDDM/giphy.gif")')
-  })
 })
