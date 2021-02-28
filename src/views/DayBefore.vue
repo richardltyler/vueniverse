e8c4308518e7a402b8d5fcef4ab9f25c9d13c414
Lucas Merchant <lbmerchant93@gmail.com>
2021-02-28 13:30:45 -0700

    Make Fetch call for day before

--- src/views/DayBefore.vue
+++ src/views/DayBefore.vue
@@ -1,24 +1,32 @@
 <template>
     <div class='day-before-container'>
         <h2>Day Before</h2>
-        <h3>{{ podb }} </h3>
+        <h3>{{ podb.date }} </h3>
     </div>
 </template>
 
 <script>
-// import apiCalls from '../apiCalls.js'
+import apiCalls from '../apiCalls.js'
+import moment from 'moment';
+
 export default {
     name: 'DayBefore',
     data() {
         return {
-            podb: "podb"
+            podb: {}
         }
     },
     methods: {
 
     },
-    created() {
-     
+    mounted() {
+        const dateMoment = moment().subtract(1, 'days').calendar('L')
+        const dateSplit = dateMoment.split('/')
+        const date = dateSplit[2] + '-' + dateSplit[0] + '-' + dateSplit[1]
+        apiCalls.getSpecificDatesPhoto(date)
+        .then(data => {
+            console.log(data)
+            this.podb = data})
     }
 }
 </script>
