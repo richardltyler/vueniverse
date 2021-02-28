4e9f84a9f7e21ee531e7a7597a03d794bda56832
colefiscus <>
2021-02-28 11:55:04 -0700

    Edit potd test file

--- cypress/integration/picture-of-the-day-spec.js
+++ cypress/integration/picture-of-the-day-spec.js
@@ -1,40 +1,48 @@
-// describe("Pic of the Day component", () => {
+describe("Pic of the Day component", () => {
 
-//   beforeEach(() => {
-    // cy.fixture('potd_data.json')
-    //   .then((response) => {
-    //     cy.intercept('https://api.nasa.gov/planetary/apod?api_key=j9VLjGbdXCRXtf61nCle9dLGtNzWVnNqUM1BNV86', {
-    //       body: response
-    //     })
-    //   })
+  beforeEach(() => {
+    cy.fixture('potd_data.json')
+      .then((response) => {
+        cy.intercept('https://api.nasa.gov/planetary/apod?api_key=j9VLjGbdXCRXtf61nCle9dLGtNzWVnNqUM1BNV86', {
+          body: response
+        })
+      })
 
-//     cy
-//       .visit("http://localhost:8080")
-//       .get("footer").get("a:last").click()
-//   })
-// })
+    cy.fixture('spec_date_data.json')
+      .then((response) => {
+        cy.intercept('https://api.nasa.gov/planetary/apod?date=2020-12-25&api_key=j9VLjGbdXCRXtf61nCle9dLGtNzWVnNqUM1BNV86', {
+          body: response
+      })
+    })
+  })
+
+    it("Should load a picture to display when navigating to the home page", () => {
+      cy
+        .visit("http://localhost:8080")
+        .get("footer").get("a:last").click()
+    })
+})
   // it("Should load a picture to display when navigating to the home page", () => {
   //   cy
   //     .get("main")
   // })
 
-describe("Should do the thing", () => {
+// describe("Should do the thing", () => {
 
-    beforeEach(() => {
-      cy.fixture('spec_date_data.json')
-        .then((response) => {
-          cy.intercept('https://api.nasa.gov/planetary/apod?date=2020-12-25&api_key=j9VLjGbdXCRXtf61nCle9dLGtNzWVnNqUM1BNV86', {
-           body: response
-        })
-      })
-      cy
-      .visit("http://localhost:8080")
-      .get("footer").get("a:last").click().wait(1000)
-    })
+//     beforeEach(() => {
+//       cy.fixture('spec_date_data.json')
+//         .then((response) => {
+//           cy.intercept('https://api.nasa.gov/planetary/apod?date=2020-12-25&api_key=j9VLjGbdXCRXtf61nCle9dLGtNzWVnNqUM1BNV86', {
+//            body: response
+//         })
+//       })
+//       cy
+//       .visit("http://localhost:8080")
+//       .get("footer").get("a:last").click().wait(1000)
+//     })
 
-    it("Should choose a date and display photo based on date", () => {
-      cy
-        .get("input").invoke("val", "2020-12-25").trigger("input")
-    })
-  })
-
+//     it("Should choose a date and display photo based on date", () => {
+//       cy
+//         .get("input").type("2020-12-25").trigger("input")
+//     })
+//   })
