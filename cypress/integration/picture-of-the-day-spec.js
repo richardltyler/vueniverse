589e6a27f8fba7c519a44a903ad50e1cf082e5c4
Richard Tyler <richardtyler2011@gmail.com>
2021-03-01 10:58:51 -0700

    Refactor POTD component to not click on home link

--- cypress/integration/picture-of-the-day-spec.js
+++ cypress/integration/picture-of-the-day-spec.js
@@ -16,17 +16,17 @@ describe("Pic of the Day component", () => {
         })
       }) 
     cy.visit("http://localhost:8080") 
+      .get("main")
+      .get("a").eq(2).click()
   })
 
   it("Should load a picture to display when navigating to the home page", () => {
     cy
-      .get("footer").get("a:last").click()
       .get("img").should("have.attr", "src", "https://apod.nasa.gov/apod/image/2102/PIA24333_fig1_1035c.jpg")
   })
 
   it("Should display a new picture after changing the date input", () => {
     cy
-      .get("footer").get("a:last").click()
       .get("header").get("input").type("2020-12-25", "{enter}").trigger("change").wait(500)
       .get("img").should("have.attr", "src", "https://www.ladbible.com/cdn-cgi/image/width=720,quality=70,format=webp,fit=pad,dpr=2/https%3A%2F%2Fs3-images.ladbible.com%2Fs3%2Fcontent%2F890e41365fd7d265d770d2c2c6969993.png")
   })
