3c5e19031eaf65bdbccec71b791d8ff9640c6d99
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-02 18:13:42 -0700

    Fix route testing

--- cypress/integration/routes-spec.js
+++ cypress/integration/routes-spec.js
@@ -1,6 +1,21 @@
+import moment from 'moment'
+
 describe("Vue Routes", () => {
+  const today = moment().format('YYYY-MM-DD')
 
   beforeEach(() => {
+    cy.intercept('GET', `https://api.nasa.gov/planetary/apod?date=${today}&api_key=j9VLjGbdXCRXtf61nCle9dLGtNzWVnNqUM1BNV86`, {
+        statusCode: 201,
+        body: {
+          "date": today,
+          "explanation":"Sometimes it do be like that.",
+          "hdurl":"https://apod.nasa.gov/apod/image/2102/PIA24333_fig1.jpg",
+          "media_type":"image",
+          "service_version":"v1",
+          "title":"Perseverance Landing Site from Mars Reconnaissance Orbiter",
+          "url":"https://apod.nasa.gov/apod/image/2102/PIA24333_fig1_1035c.jpg"
+        }
+      })
     cy.visit("http://localhost:8080")
   })
   
@@ -12,23 +27,23 @@ describe("Vue Routes", () => {
     cy
       .get("footer a").click()
       cy.url().should("include", "/about")
-      .get("header a").eq(1).click()
-      cy.url().should("include", "/home")
+      .get("header a").click()
+      cy.url().should("include", `/date/${today}`)
   })
 
   it("Should change to the About URL when navigating to the About component", () => {
     cy
-      .get("header a").eq(1).click()
-    cy.url().should("include", "/home")
-    cy.url().should("include", "/home")
+      .get("header a").click()
+    cy.url().should("include", `/date/${today}`)
+
       .get("footer a").click()
     cy.url().should("include", "/about")
   })
 
   it("Should navigate to the Home URL when clicking on the Vueniverse headline in the header", () =>{
     cy
-      .get("header a:first").click();
-    cy.url().should("include", "/home");
+      .get("header a").click();
+    cy.url().should("include", `/date/${today}`);
   })
 })
 
